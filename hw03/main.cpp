b1278e49a6019440566126db0ce5e7be2b5ee139
Petr Laštovička <lastope2@fit.cvut.cz>
2022-12-06 18:34:23 +0100

    hw03: Better data structures used

--- hw03/main.cpp
+++ hw03/main.cpp
@@ -33,124 +33,72 @@ using namespace std;
 
 using Args = TreeProblem;
 using Point = ChristmasTree;
-using Map = unordered_multimap<Point, Point>;
+using Map = vector<vector<Point>>;
+using BackMap = vector<Point>;
+using StartingList = vector<Point>;
 using Cache = vector<size_t>;
 
-struct InversedTree {
+struct Tree {
   const Map map;
-  const vector<Point> starting;
+  const BackMap back;
+  const StartingList starting;
 };
 
 
 const Point start = 0;
 
-void printMap(const Map& map, ostream& out = cout) {
+template<typename T>
+ostream& operator<<(ostream& out, const vector<T>& data) {
   bool isFirst = true;
-  out << "{";
-  for (const auto& vert : map) {
-
+  out << "(";
+  for (const auto& target: data) {
     if (isFirst) isFirst = false;
     else out << ",";
 
-    out << "(" << vert.first << "," << vert.second << ")";
+    out << target;
   }
-  out << "}" << endl;
+  return out << ")";
 }
 
+inline Map buildVerticies(const Args& args) {
+  Map map(args.gifts.size());
 
-Map buildVerticies(const Args& args) {
-  Map map;
   for (const auto& conn : args.connections) {
-    map.emplace(conn);
-    map.emplace(make_pair(conn.second, conn.first));
+    map[conn.first].emplace_back(conn.second);
+    map[conn.second].emplace_back(conn.first);
   }
+
   return map;
 }
 
-Map buildTree(const Map& vert) {
-  Map out;
+Tree buildTree(const Args& args, const Map& vert) {
+  const size_t totalItems = args.gifts.size();
+  Map map(totalItems);
+  BackMap back(totalItems);
+  StartingList starting;
 
   queue<pair<Point, Point>> q;
-  q.emplace(make_pair(start, (Point) -1));
+  const size_t invalidState = (Point) -1;
+  q.emplace(make_pair(start, invalidState));
 
   while (!q.empty()) {
     const auto [item, parent] = q.front();
     q.pop();
 
-    const auto range = vert.equal_range(item);
-    for (auto itr = range.first; itr != range.second; ++itr) {
-
-      const Point curr = itr -> second;
+    for (const auto curr : vert[item]) {
       if (curr == parent) continue;
 
-      out.emplace(make_pair(item, curr));
+      map[item].emplace_back(curr);
+      back[curr] = item;
       q.emplace(make_pair(curr, item));
     }
-  }
 
-  return out;
-}
-
-InversedTree inverseTree(const Map& tree) {
-  Map out;
-  vector<Point> starting;
-
-  queue<Point> q;
-  q.emplace(start);
-
-  while (!q.empty()) {
-    const Point item = q.front();
-    q.pop();
-
-    const auto range = tree.equal_range(item);
-    for (auto itr = range.first; itr != range.second; ++itr) {
-
-      const Point curr = itr -> second;
-
-      out.emplace(make_pair(curr, item));
-      q.emplace(curr);
-    }
-
-    // no children
-    if (range.first == range.second) {
+    if (map[item].empty()) {
       starting.emplace_back(item);
     }
   }
 
-  return InversedTree { move(out), move(starting) };
-}
-
-size_t resolveNode(
-    const Args& args,
-    const Map& tree,
-    Cache& cache,
-    const Point node,
-    const bool shouldGuard
-    ) {
-
-  const size_t cacheIndex = node + (shouldGuard ? args.gifts.size() : 0);
-  if (cache[cacheIndex] != 0)
-    return cache[cacheIndex] - 1;
-
-  size_t max = 0;
-
-  const auto range = tree.equal_range(node);
-  for (auto itr = range.first; itr != range.second; ++itr) {
-    const Point curr = itr -> second;
-
-    size_t r = resolveNode(args, tree, cache, curr, false);
-
-    if (!shouldGuard) {
-      const size_t r2 = resolveNode(args, tree, cache, curr, true);
-      if (r2 > r) r = r2;
-    }
-    max += r;
-  }
-
-  const size_t res = max + (shouldGuard ? args.gifts[node] : 0);
-  cache[cacheIndex] = res + 1;
-
-  return res;
+  return Tree{ move(map), move(back), move(starting) };
 }
 
 void printCache(const Cache& cache, size_t rowWidth, ostream& out = cout) {
@@ -167,97 +115,28 @@ void printCache(const Cache& cache, size_t rowWidth, ostream& out = cout) {
   }
 }
 
-size_t resolveNodeIterativeBFS(
-    const Args& args,
-    const Map& tree,
-    const InversedTree& inversed
-) {
-
-  const size_t totalItems = args.gifts.size();
-  const Map& map = inversed.map;
-  const vector<uint64_t>& gifts = args.gifts;
-
-  Cache cache((1 + args.max_group_size) * totalItems);
-
-  queue<Point> q;
-  for (const size_t node : inversed.starting) {
-    q.emplace(node);
-  }
-
-  while (!q.empty()) {
-    const Point item = q.front();
-    q.pop();
-
-    if (cache[item] != 0)
-      continue;
-
-    // printCache(cache, totalItems);
-    // cout << "Handling " << item << endl;
-
-    size_t sumNo = 0; // not guarded
-    size_t sumWith = gifts[item]; // guarded
-
-    bool cacheMiss = false;
-    // compute value
-    const auto range = tree.equal_range(item);
-    for (auto itr = range.first; itr != range.second; ++itr) {
-      const Point curr = itr -> second;
-
-      size_t r = cache[curr] - 1;
-      // cache miss, return to queue end
-      if (r == (Point) -1) {
-        // cout << "Skipping " << item << endl;
-        q.emplace(item);
-        cacheMiss = true;
-        break;
-      }
-
-      sumWith += r;
-
-      size_t r1 = cache[curr + totalItems] - 1;
-      sumNo += r > r1 ? r : r1;
-    }
-
-    if (cacheMiss) {
-      continue;
-    }
-
-    cache[item] = sumNo + 1;
-    cache[item + totalItems] = sumWith + 1;
-
-    {
-      // schedule next ones
-      const auto range = map.equal_range(item);
-      for (auto itr = range.first; itr != range.second; ++itr) {
-        const Point curr = itr -> second;
-        q.emplace(curr);
-      }
-    }
-  }
-
-  // printCache(cache, totalItems);
-  return (cache[start] > cache[start + totalItems]
-      ? cache[start] : cache[start + totalItems]) - 1;
-}
-
-
 size_t resolveNodeIterativeTopSort(
     const Args& args,
-    const Map& tree,
-    const InversedTree& inversed
+    const Tree& tree
 ) {
-
   const size_t totalItems = args.gifts.size();
-  const Map& map = inversed.map;
+
+  const Map& map = tree.map;
+  const BackMap& back = tree.back;
+  const StartingList& starting = tree.starting;
   const vector<uint64_t>& gifts = args.gifts;
 
   Cache cache((1 + args.max_group_size) * totalItems);
 
   queue<Point> q;
-  vector<size_t> topSortArray(totalItems); 
-  for (const auto& edge : tree) {
-    ++topSortArray[edge.first];
+
+  vector<uint32_t> topSortArray(totalItems); 
+
+  for (const auto& edge : back) {
+    ++topSortArray[edge];
   }
+  --topSortArray[0];
+  // cout << "Top " << topSortArray << endl;
 
   // initial Top Sort fill up
   // for (Point p = 0; p < totalItems; ++p) {
@@ -265,7 +144,7 @@ size_t resolveNodeIterativeTopSort(
   //     q.emplace(p);
   //   }
   // }
-  for (const Point p : inversed.starting) {
+  for (const Point p : starting) {
     q.emplace(p);
   }
 
@@ -280,9 +159,7 @@ size_t resolveNodeIterativeTopSort(
     size_t sumWith = gifts[item]; // guarded
 
     // compute value
-    const auto range = tree.equal_range(item);
-    for (auto itr = range.first; itr != range.second; ++itr) {
-      const Point curr = itr -> second;
+    for (const auto& curr : map[item]) {
 
       const size_t r = cache[curr] - 1;
       sumWith += r;
@@ -294,15 +171,13 @@ size_t resolveNodeIterativeTopSort(
     cache[item] = sumNo + 1;
     cache[item + totalItems] = sumWith + 1;
 
+
     {
       // schedule next ones
-      const auto range = map.equal_range(item);
-      for (auto itr = range.first; itr != range.second; ++itr) {
-        const Point curr = itr -> second;
-        --topSortArray[curr];
-        if (topSortArray[curr] == 0) {
-          q.emplace(curr);
-        }
+      const Point curr = back[item];
+      --topSortArray[curr];
+      if (topSortArray[curr] == 0) {
+        q.emplace(curr);
       }
     }
   }
@@ -316,20 +191,11 @@ uint64_t solve(const Args& args) {
   if (args.gifts.size() == 0)
     return 0;
 
-  const Map tree = buildTree(buildVerticies(args));
-
-  // Cache cache((1 + args.max_group_size) * args.gifts.size());
-  //
-  // size_t max1 = resolveNode(args, tree, cache, 0, true);
-  // size_t max2 = resolveNode(args, tree, cache, 0, false);
-  //
-  //return max1 > max2 ? max1 : max2;
-
-  // const InversedTree inversed = inverseTree(buildTree(buildVerticies(args)));
-  // return resolveNodeIterativeBFS(args, tree, inversed);
-
-  const InversedTree inversed = inverseTree(buildTree(buildVerticies(args)));
-  return resolveNodeIterativeTopSort(args, tree, inversed);
+  const Tree tree = buildTree(args, buildVerticies(args));
+  // cout << tree.map << endl;
+  // cout << tree.back << endl;
+  // cout << tree.starting << endl;
+  return resolveNodeIterativeTopSort(args, tree);
 }
 
 #ifndef __PROGTEST__
